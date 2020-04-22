670fc4b3f018c0b8f28a702c1057502b868e32b1
Jason <shty1990@163.com>
2020-04-22 14:22:52 +0800

    fix

--- src/add-npm-downloads-count-to-search-results.js
+++ src/add-npm-downloads-count-to-search-results.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Add downloads count for npm search results
 // @namespace    http://tampermonkey.net/
-// @version      0.1.0
+// @version      0.1.1
 // @description  Add downloads count for npm search results
 // @author       sososoyoung
 // @connect      api.npmjs.org
@@ -9,15 +9,20 @@
 // @include      *://www.npmjs.com/search*
 // @require      https://cdn.staticfile.org/jquery/3.4.1/jquery.min.js
 // @grant        GM.xmlHttpRequest
+// @grant        GM.log
 // @license      MIT
 // ==/UserScript==
-
 //@match        http://www.npmjs.com
-(function() {
+
+(function () {
   "use strict";
 
   let href = "";
+  let packageName = "";
   let checkWorkId = Date.now();
+  const selecters = {
+    des: ".w-80 .black-80 .black-50",
+  };
 
   function getSections() {
     const sections = $("section");
@@ -33,29 +38,32 @@
       GM.xmlHttpRequest({
         method: "GET",
         headers: {
-          Accept: "application/json" // If not specified, browser defaults will be used.
+          Accept: "application/json", // If not specified, browser defaults will be used.
         },
         url:
           "https://api.npmjs.org/downloads/point/last-week/" +
           encodeURIComponent(name),
-        onload: function(res) {
+        onload: function (res) {
           r(JSON.parse(res.responseText).downloads);
         },
-        onerror: j
+        onerror: j,
       });
     });
   }
 
   function getPkgName(section) {
-    const name = $(section)
-      .find(".w-80 .items-end h3")
-      .text();
+    const name = $(section).find(".w-80 .items-end h3").text();
     return name;
   }
 
   function setCount(section, count) {
-    const desDom = $(section).find(".w-80 .black-80 .black-50");
+    const desDom = $(section).find(selecters.des);
     desDom.html(desDom.html() + " • Weekly Downloads: " + count);
+  }
+
+  function checkHadCount(section) {
+    const desDom = $(section).find(selecters.des);
+    return /Weekly Downloads/.test(desDom.html());
   }
 
   const splitCount = [1000 * 1000, 1000];
@@ -72,7 +80,7 @@
     setCount(section, count);
   }
 
-  async function start(id) {
+  async function updateCounts(id) {
     try {
       const s = Date.now();
       href = window.location.href;
@@ -82,38 +90,53 @@
         sections.length > 0 &&
         window.location.pathname === "/search"
       ) {
-        console.log("[d-count] start...");
+        GM.log("[d-count] updateCounts...");
         for (let i = 0; i < sections.length; i++) {
-          if (id === checkWorkId) {
+          if (id === checkWorkId && !checkHadCount(sections[i])) {
             await addDownloadCount(sections[i]);
           } else {
-            console.log("[d-count] continue.");
+            GM.log("[d-count] continue.");
             continue;
           }
         }
       } else {
-        console.log("[d-count] PASS.");
+        GM.log("[d-count] PASS.");
       }
-      console.log(
+      GM.log(
         "[d-count] done. Take: " + Math.floor((Date.now() - s) / 1000) + "s."
       );
-    } catch (error) {
-      console.error("start error:", e);
+    } catch (e) {
+      console.error("updateCounts error:", e);
     }
   }
 
+  function parseSeacrh(search = "") {
+    return search
+      .split("&")
+      .map((i = "") => i.split("="))
+      .reduce((o, i) => {
+        if (i.length === 2 && i[0]) {
+          o[i[0]] = i[1];
+        }
+        return o;
+      }, {});
+  }
+
   function checkUrl() {
-    if (href !== window.location.href) {
-      console.log("[d-count] url had changed!!!");
+    if (href !== window.location.href && window.location.search) {
+      const newQ = parseSeacrh(window.location.search.substr(1)).q;
+      GM.log("[d-count] url had changed!", newQ, packageName);
+      packageName = newQ;
       checkWorkId = Date.now();
-      start(checkWorkId);
+      updateCounts(checkWorkId);
     }
   }
+
   function checkUrlChange() {
     setInterval(checkUrl, 500);
   }
 
   // start
-  start(checkWorkId);
+  updateCounts(checkWorkId);
   checkUrlChange();
 })();
